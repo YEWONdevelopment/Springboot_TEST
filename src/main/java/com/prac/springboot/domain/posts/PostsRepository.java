1a827cb84e5d2868efda019190397fee547ea617
yewon <cathy970816@gmail.com>
2022-04-04 01:04:20 +0900

    게시판 등록

--- src/main/java/com/prac/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/prac/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package com.prac.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 //Entity클래스와 기본 Entity Repository는 같은 선상에 위치해야함
 public interface PostsRepository extends JpaRepository<Posts,Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/prac/springboot/service/posts/PostsService.java
+++ src/main/java/com/prac/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.prac.springboot.service.posts;
 
 import com.prac.springboot.domain.posts.Posts;
 import com.prac.springboot.domain.posts.PostsRepository;
+import com.prac.springboot.web.dto.PostsListResponseDto;
 import com.prac.springboot.web.dto.PostsResponseDto;
 import com.prac.springboot.web.dto.PostsSaveRequestDto;
 import com.prac.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor        //생성자 주입 1)순환 참조 방지, 2)테스트코드 작성용이 3)코드 악취 제거 4)객체변이 방지(final 가능)
 @Service
@@ -31,9 +34,23 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다.id="+id));
+        postsRepository.delete(posts);
+    }
+
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(() ->
                 new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)     //트랜잭션 범위는 유지하되, 조회기능만 남겨두어 조회속도가 개선 / 등록,수정,삭제는 전혀X
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)     //.map(posts->new PostsListResponseDto(posts))와 같음
+                .collect(Collectors.toList());
     }
 }
