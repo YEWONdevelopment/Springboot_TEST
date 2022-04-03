c6ccae22e047f8fd34d1d48fa0e8ac008faf8039
yewon <cathy970816@gmail.com>
2022-04-03 20:55:33 +0900

    게시판 수정,조회 테스트코드 만들기/JPA Auditing으로 생성,수정시간 자동화

--- src/main/java/com/prac/springboot/domain/posts/Posts.java
+++ src/main/java/com/prac/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.prac.springboot.domain.posts;
 
+import com.prac.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,7 @@ import javax.persistence.*;
 @Getter             //클래스 내 모든 필드의 Getter 메소드를 자동생성
 @NoArgsConstructor  //기본 생성자 자동추가, public Posts(){}와 같은 효과
 @Entity             //테이블과 링크될 클래스, 기본값으로 클래스의 이름을 테이블 이름으로 매칭, 절대 Setter메소드 만들지 않음
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id         //PK를 나타냄
     @GeneratedValue(strategy = GenerationType.IDENTITY)     //PK생성규칙
@@ -28,5 +29,10 @@ public class Posts {
         this.title = title;
         this.content = content;
         this.author = author;
+    }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
     }
 }
--- src/test/java/com/prac/springboot/domain/posts/PostsrepositoryTest.java
+++ src/test/java/com/prac/springboot/domain/posts/PostsrepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -42,8 +43,28 @@ public class PostsrepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
 
+    @Test
+    public void BaseTimeEntity_insert() {
+        //given
+        LocalDateTime now = LocalDateTime.of(2022,4,3,20,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
 
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>CreateDate"+posts.getCreatedDate()+",modifiedDate="+posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 
 }
